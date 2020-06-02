6bad3ee89b1d99103843fd62ef863a9b6c1c1de1
Robert Hill <hill.rws@gmail.com>
2020-06-02 16:07:23 +0200

    add default value to listing field 'active'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_02_102127) do
+ActiveRecord::Schema.define(version: 2020_06_02_140020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2020_06_02_102127) do
     t.string "care_level_category"
     t.string "listing_type"
     t.text "description"
-    t.boolean "active"
+    t.boolean "active", default: true
     t.integer "quantity", default: 1
     t.string "postcode"
     t.string "city"
