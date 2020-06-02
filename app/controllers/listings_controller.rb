ee4f74f5a4b5c76dc6ae57673965ba36c8c9401e
Odeta Maslovaite <odetamaslovaite@yahoo.com>
2020-06-02 12:56:01 +0200

    in porgress

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,13 @@
 class ListingsController < ApplicationController
+skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def edit
+    @listing = Listing.find(params[:id])
+    authorize @listing
+  end
+
+  def show
+    @listing = Listing.find(params[:id])
+    authorize @listing
+  end
 end
--- app/policies/listing_policy.rb
+++ app/policies/listing_policy.rb
@@ -4,4 +4,9 @@ class ListingPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def show?
+    true
+  end
+
 end
