644ae9af800f3fd40b0b34801cc157e12b16e197
Robert Hill <hill.rws@gmail.com>
2020-06-05 00:59:32 +0200

    making progress

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,3 +1,23 @@
 class ChatroomsController < ApplicationController
-  @chatroom = Chatroom.find(params[:id])
+  before_action :set_chatroom, only: :show
+
+  def new
+    @chatroom = Chatroom.new
+    @chatroom.user = current_user
+    @chatroom.listing = @listing
+    @chatroom.status = "pending"
+    @chatroom.save
+    render :show
+    authorize @chatroom
+    # redirect_to @chatroom
+  end
+  
+  def show
+  end
+
+  def set_chatroom
+    @chatroom = Chatroom.find(params[:id])
+    authorize @chatroom
+  end
+  
 end
