df83860586f9e952d54ce07668fc5e605ed4205b
Robert Hill <hill.rws@gmail.com>
2020-06-05 13:03:17 +0200

    steps in the right direction

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,22 +1,21 @@
 class ChatroomsController < ApplicationController
   before_action :set_chatroom, only: :show
 
-  def new
+  def create
     @chatroom = Chatroom.new
     @chatroom.user = current_user
     @chatroom.listing = Listing.find(params[:listing_id])
     @chatroom.status = "pending"
     @chatroom.save
     authorize @chatroom
-
-    render :show
+    # render :show
+    redirect_to chatroom_path(@chatroom)
     # ?redirect_to @chatroom
   end
   
   def show
     @message = Message.new
-    authorize @message
-    # I also created a message policy.. required? TODO: If not required, delete message policy
+    # also created a message policy.. required? TODO: If not required, delete message policy
     # without the message form, it "works"
   end
 
