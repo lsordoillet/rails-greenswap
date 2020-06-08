cbfcf3f67a102690303ea07b8a40fcb28054e822
Léa Sordoillet <62567607+lsordoillet@users.noreply.github.com>
2020-06-08 12:55:57 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :listings do
     resources :chatrooms, only: :create  # which ones do we need? chatroom lecture only has show, but I guess we need most of these?
-
-    # right now its http://localhost:3000/listings/2/chatrooms/1
-    # but does this make sense? can a listing have many chatrooms/bookings? I guess yes, because multiple people could start a booking for the same listing. 
-    # what if seller confirms multiple bookings for the same listing?
-
-    # TODO: is double nesting ok??
     
+    # what if seller confirms multiple bookings for the same listing?
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/profile", to: 'dashboard#profile'
