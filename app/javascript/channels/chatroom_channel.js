933bdbc0c8e9d46a6bb37773ff469871f28be1aa
Robert Hill <hill.rws@gmail.com>
2020-06-08 11:35:57 +0200

    removed commented code, changed button logic, changed show page

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -7,23 +7,10 @@ const initChatroomCable = () => {
 
     consumer.subscriptions.create({ channel: "ChatroomChannel", id: id }, {
       received(data) {
-        messagesContainer.insertAdjacentHTML('beforeend', data);
+        messagesContainer.insertAdjacentHTML('beforeend', data); 
       }
     });
   }
 }
-
-// import consumer from "./consumer";
-
-// const messagesContainer = document.getElementById('messages');
-// if (messagesContainer) {
-//   const id = messagesContainer.dataset.chatroomId;
-
-//   consumer.subscriptions.create({ channel: "ChatroomChannel", id: id }, {
-//     received(data) {
-//       messagesContainer.insertAdjacentHTML('beforeend', data); // called when data is broadcast in the cable
-//     }
-//   });
-// }
 
 export { initChatroomCable };
