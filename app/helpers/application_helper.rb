a756a383c485c7f7e342e85fe5eb74b876e2cf1c
lea sordoillet <leasordoillet@gmail.com>
2020-06-10 10:31:03 +0200

    elsif more than 4 pictures

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,15 @@
 module ApplicationHelper
   def grid_builder(photos_collection)
-   if photos_collection.size == 4
+    if photos_collection.size == 4
       "four-images-grid"
     elsif photos_collection.size == 3
       "three-images-grid"
     elsif photos_collection.size == 2
       "two-images-grid"
+    elsif photos_collection.size == 1
+       "one-image-grid"
     else
-      "one-image-grid"
+      "four-images-grid"
     end
   end
 end
